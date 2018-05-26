f78edf887df7db9a7cdbf00321e624bb84634f62
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-05-26 19:31:29 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Webpack 4 starter
+
+To start development of new landing site
+
+````
+npm i
+npm start
+npm run build
+````
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const conf = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader',
+        // options: {
+        //   pretty: true
+        // }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      }, {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  autoprefixer({
+                    browsers:['ie >= 8', 'last 4 version']
+                  })
+                ],
+                // sourceMap: true
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }, {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/',
+            publicPath: 'img/'
+          }
+        }]
+      },
+      // ===== FONTS =====
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.pug'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+};
+
+module.exports = (env, options) => {
+  if (options.mode !== 'production') {
+    conf.devtool = 'eval-sourcemap';
+  }
+  return conf;
+};
