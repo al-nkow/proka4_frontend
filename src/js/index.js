44adb64c812f896cd4ac4bbebe2309c00dbe4fdf
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-06-03 18:29:45 +0300

    faq

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/style.scss';
 import 'ubuntu-fontface/_ubuntu.scss'
 
+import Siema from 'siema';
+
+
+
+const newsSiema = new Siema({
+  selector: '.newsSiema',
+  perPage: {
+    400: 1,
+    800: 2, // 2 items for viewport wider than 800px
+    1240: 3
+  },
+  loop: true
+});
+document.querySelector('.news-prev').addEventListener('click', () => newsSiema.prev());
+document.querySelector('.news-next').addEventListener('click', () => newsSiema.next());
+
+
+
+
 // import $ from 'jquery';
 
 const testFunc = (a, b) => {
