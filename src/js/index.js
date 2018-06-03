4d3d1dfb6db61b3ff69ada9cc1f220ec260339f7
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-06-03 15:52:56 +0300

    results

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/style.scss';
 import 'ubuntu-fontface/_ubuntu.scss'
 
+// import $ from 'jquery';
+
 const testFunc = (a, b) => {
   console.log('>>>>>', a, b);
 };
@@ -14,3 +16,6 @@ let b = 6;
 testFunc(a, b);
 
 console.log('HELLO!!!', last);
+
+
+// $('.carousel').carousel();
